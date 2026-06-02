c498f72b46c51ef191501183721bdd3b865ea9e9
ShaikMazher <mazhers714@example.com>
2026-06-03 00:00:15 +0530

    Solve Contains Duplicate in O(n) using Hash Set

--- Week-01/01_contains_duplicate.py
+++ Week-01/01_contains_duplicate.py
@@ -8,3 +8,16 @@ def containsDuplicate(nums):
 # Test Cases for tomorrow morning
 print(containsDuplicate([1, 2, 3, 1]))  # Expected output: True
 print(containsDuplicate([1, 2, 3, 4]))  # Expected output: False
+
+# leetcode problem number (#217)
+#217. Contains Duplicate Given an integer array nums, return true if any value appears at least twice in the array
+# , and return false if every element is distinct.
+
+def contideupicate(num):
+    for L in range(len(num)):
+        for R in range(L+1,len(num)):
+            if num[L]==num[R]:
+                return True
+    return False
+print(contideupicate([1,2,3,1]))
+print(contideupicate([1,2,3,4]))
